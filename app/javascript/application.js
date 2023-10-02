f84b94884c8363ef91f09d3dc4c4d6418fab2dd0
ProgramKingAbel <abelmorara254@gmail.com>
2023-10-02 15:42:44 +0300

    Generate controller to serve react app

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom/client';
+import App from './components/App.jsx';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+);
